67195deca003dcb92b763c2367de9692b3692b29
Richard Joe <rjoe19@hotmail.com>
2016-09-30 12:56:01 +1300

    added delete http req

--- server.js
+++ server.js
@@ -39,6 +39,8 @@ router.get('/', function(req,res) {
 });
 
 // we will add more routes for our API here...
+// on routes that end in /bears...including post and get here
+//--------------------------------------------------------------------------
 
 router.route('/bears')
 
@@ -68,6 +70,50 @@ router.route('/bears')
     });
 
 
+// on routes that end in /bears/:bear_id
+//--------------------------------------------------------------------------
+    router.route('/bears/:bear_id')
+
+      // get the bear with that id (accessed at GET http://localhost:8080/api/bears/bear_id)
+      .get(function(req, res) {
+        Bear.findById(req.params.bear_id, function(err, bear) {
+            if (err)
+                res.send(err);
+            res.json(bear);
+        });
+      });
+
+              // update the bear with this id (accessed at PUT http://localhost:8080/api/bears/:bear_id)
+              // use our bear model to find the bear we want
+              Bear.findById(req.params.bear_id, function(err, bear) {
+
+              if (err)
+                  res.send(err);
+
+              bear.name = req.body.name; //update the bears info
+
+              // save the bear
+              bear.save(function(err) {
+                  if (err)
+                      res.send(err);
+
+                  res.json({message: 'Bear updated!'});
+              });
+          });
+
+          // delete the bear with this id (accessed at DELETE http://localhost:8080/api/bears/:bear_id)
+          .delete(function(req, res) {
+              Bear.remove({
+                  _id:req.params.bear_id
+              }, function(err, bear) {
+                  if (err)
+                      res.send(err);
+
+                  res.json({message: 'Successfully deleted'});
+              });
+          });
+
+          
 
 //REGISTER THE ROUTES
 // all routes will be prefixed with /api
